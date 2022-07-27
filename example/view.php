5fa74f9552ff5357e5ecadd03de9803502e5a604
Edi Haryono <energipositif@gmail.com>
2022-07-28 00:19:27 +0700

    Create view.php

---
+++ example/view.php
@@ -0,0 +1,13 @@
+<div role="document" class="modal-dialog modal-md modal-detail">
+    <div class="modal-content">
+ 
+            <div  class="modal-body"><!---->
+                <div   class="margin-top-40 margin-bottom-40">
+                    <h4  style="text-align: center;">Menyalin jadwal</h4>
+                    <p  style="text-align: center;">Harap menunggu untuk beberapa saat</p>
+                    <div  class="progress-barnya">
+                        <div  class="progressnya" style="width: 48.7374%;"></div></div>
+                        <p  style="text-align: center;">Mengunduh data...</p></div><!----></div>
+ 
+</div>
+</div>
